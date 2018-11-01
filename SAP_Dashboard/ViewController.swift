c9d251e4bd110e9332e70d84e8e2cdd6aad22008
s0554822@htw-berlin.de <s0554822htw-berlin.de@wlan-141-23-181-221.tubit.tu-berlin.de>
2018-11-01 17:07:09 +0100

    Initial Commit

---
+++ SAP_Dashboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SAP_Dashboard
+//
+//  Created by s0554822@htw-berlin.de on 01.11.18.
+//  Copyright © 2018 s0554822@htw-berlin.de. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
